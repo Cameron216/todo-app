1f684ba9904cc71b278fb9829e46d3649ba8e1bf
Cameron Bailey <CameronBailey216@hotmail.com>
2019-12-05 18:10:23 +0000

    Added some new components and a bit more of the projects folder structure.

---
+++ client/src/components/ToDos/ToDos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ToDos = () => {
+    return (<h1>ToDos</h1>);
+}
+
+export default ToDos;
---
+++ client/src/components/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <div className="App"></div>
+        </Router>
+    );
+}
+
+export default App;
